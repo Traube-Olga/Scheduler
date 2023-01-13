44172539409b131e6a0ee14229694c267a497334
Selarow <selarowwastaken@gmail.com>
2023-01-13 09:01:41 +0100

    RR implementation

--- scheduler.py
+++ scheduler.py
@@ -15,7 +15,7 @@ def read_jobs(file):
 
     for line in lines:
         list_line = list(map(int, line.strip().split(" ")))
-        id = float(list_line[0])
+        id = (list_line[0], 0)
         jobs[id] = list_line[1:]
 
     return jobs
@@ -61,7 +61,7 @@ def write_results(schedule, file):
     f.write("#jobid serverid start end frequency\n")
 
     for k, v in schedule.items():
-        line = f"{k} {v[0]} {v[1]} {v[2]} {v[3]}\n"
+        line = f"{k[0]} {v[0]} {v[1]} {v[2]} {v[3]}\n"
         f.write(line)
     
     f.close()
@@ -74,12 +74,8 @@ def add_periodic_jobs(jobs, repeat):
         period = v[3]
 
         if period != 0:
-
-            cnt = 0.0
-
             for i in range(1, repeat + 1):
-                cnt += 0.1
-                new_id = k + cnt
+                new_id = (k[0], i)
                 new_arrival_date = period * i + v[0]
                 new_deadline = new_arrival_date + v[2]
                 new_value = [new_arrival_date, v[1], new_deadline, 0]
@@ -88,8 +84,7 @@ def add_periodic_jobs(jobs, repeat):
     return jobs | periodic_jobs
 
 
-def FIFO(jobs, servers, dependencies, repeat):
-    jobs = add_periodic_jobs(jobs, repeat)
+def FIFO(jobs, servers, dependencies):
     sorted_jobs = {k: v for k, v in sorted(jobs.items(), key=lambda item: item[1][0])}  # Sort jobs by arrival date
     schedule = dict()
     server_id = 0
@@ -112,15 +107,53 @@ def FIFO(jobs, servers, dependencies, repeat):
     return schedule
 
 
-def RR(jobs, servers, dependencies):
-    pass
+def RR(jobs, servers, dependencies, quantum):
+    sorted_jobs = {k: v for k, v in sorted(jobs.items(), key=lambda item: item[1][0])}  # Sort jobs by arrival date
+    schedule = dict()
+    job_ids = [k for k in jobs.keys()]
+    server_id = 0
+    frequency = 1
+    queue = []
+    next_arrival = 0
+    current_date = 0
+    cnt = 0
+
+    while job_ids:
+        new_job = job_ids.pop(0)
+        queue.append(new_job)
+
+        if job_ids:
+            next_job = job_ids[0]
+            next_arrival = sorted_jobs[next_job][0]
+        else:
+            next_arrival = -1
+
+        while next_arrival > current_date and queue:
+            current_job = queue.pop(0)
+            w = sorted_jobs[current_job][1]
+            start = current_date
+
+            if w > quantum:
+                sorted_jobs[current_job][1] -= quantum
+                current_date += quantum
+                queue.append(current_job)
+
+            else:
+                current_date += w
+            
+            schedule[(current_job[0], cnt)] = [server_id, start, current_date, frequency]
+            cnt += 1
+    
+    return schedule
 
 
 
 if __name__ == "__main__":
     jobs = read_jobs(JOB_FILE)
+    jobs = add_periodic_jobs(jobs, REPEAT)
     servers = read_servers(SERVER_FILE)
     dependencies = read_dependencies(DEPENDENCY_FILE)
-    FIFO_schedule = FIFO(jobs, servers, dependencies, REPEAT)
+    FIFO_schedule = FIFO(jobs, servers, dependencies)
     write_results(FIFO_schedule, "FIFOschedule.txt")
-    
+    RR_schedule = RR(jobs, servers, dependencies, 5)
+    write_results(RR_schedule, "RRschedule.txt")
---
+++ test.py
@@ -0,0 +1,7 @@
+a = {1: "a", 2: "b", 3: "c"}
+print(next(iter(a.keys())))
+print(next(iter(a.keys())))
+print(next(iter(a.keys())))
+print(next(iter(a.keys())))
+print(next(iter(a.keys())))
+print(next(iter(a.keys())))
